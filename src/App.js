78dbff2ea8e881e4b577bfba3a477e99474b44bc
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-08-30 17:30:24 -0400

    added a background, added a bear icon, imported some custom pics, renamed title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 import Minter from './Minter'
+import pixelb2 from './pixelb2.png'
 
 function App() {
     return (
-        <div className="App">
+        <div style={{
+            backgroundImage: `url(${pixelb2})`, backgroundPosition: 'center', backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+        }}  className="App">
             <Minter></Minter>
         </div>
     );
