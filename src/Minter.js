1fc906aa28d8dd85476a3d03ed43fb8bd2e6800a
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-09-07 12:06:28 -0400

    added a wallet listener incase there are multiple wallets for a metamask user and also called the function in useeffect

--- src/Minter.js
+++ src/Minter.js
@@ -15,7 +15,34 @@ const Minter = (props) => {
         const { address, status } = await getCurrentWalletConnected();
         setWallet(address)
         setStatus(status);
+
+        addWalletListener();
     }, []);
+
+    function addWalletListener() {
+        if (window.ethereum) {
+            window.ethereum.on("accountsChanged", (accounts) => {
+                if (accounts.length > 0) {
+                    setWallet(accounts[0]);
+                    setStatus("👆🏽 Write a message in the text-field above.");
+                } else {
+                    setWallet("");
+                    setStatus("🦊 Connect to Metamask using the top right button.");
+                }
+            });
+        } else {
+            setStatus(
+                <p>
+                    {" "}
+                    🦊{" "}
+                    <a target="_blank" href={`https://metamask.io/download.html`}>
+                        You must install Metamask, a virtual Ethereum wallet, in your
+                        browser.
+                    </a>
+                </p>
+            );
+        }
+    }
 
     const connectWalletPressed = async () => {
         const walletResponse = await connectWallet();
